4f7993d82af9611c5b0d7e5943d95417f2897b85
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-12 14:02:29 -0600

    Add ApplicationContext to Program.cs file

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace BookStore
+﻿using BookStore.Data;
+
+namespace BookStore
 {
     internal class Program
     {
+        public static ApplicationContext DbContext() => new ApplicationContextFactory().CreateDbContext(); 
         static void Main(string[] args)
         {
-            
+            using (ApplicationContext db = DbContext())
+            {
+
+            }
         }
     }
 }
