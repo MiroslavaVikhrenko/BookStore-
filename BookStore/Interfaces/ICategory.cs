6dce2a6cee5b446f42ba7c91f081831d4c6291c0
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-12 16:12:36 -0600

    Add ICategory interface

---
+++ BookStore/Interfaces/ICategory.cs
@@ -0,0 +1,19 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Interfaces
+{
+    public interface ICategory
+    {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category> GetCategoryAsync(int id);
+
+        Task AddCategoryAsync(Category category);
+        Task EditCategoryAsync(Category category);
+        Task DeleteCategoryAsync(Category category);
+    }
+}
