3e585cedb5197df6f26dad73304c9d8973ed6841
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-12 13:35:33 -0600

    Install packages and create ApplicationContext class

---
+++ BookStore/Data/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options){ }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasMany<Author>(b => b.Authors)
+                .WithMany(a => a.Books)
+                .UsingEntity(e => e.ToTable("BookAuthor"));
+        }
+    }
+}
