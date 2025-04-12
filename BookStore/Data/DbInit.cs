e53d2e00a2eafadafbf4c96ec87e1ea71e52fad3
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-12 14:31:07 -0600

    Add DbInit and AuthorRepository classes

---
+++ BookStore/Data/DbInit.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Data
+{
+    public class DbInit
+    {
+        public void Init(ApplicationContext context)
+        {
+            if (!context.Authors.Any())
+            {
+                context.Authors.AddRange(new Author[]
+                {
+                    new Author {Name = "Haruki Murakami" },
+                    new Author {Name = "Natsume Soseki"},
+                    new Author {Name = "Kawabata Yasunari"},
+                    new Author {Name = "Ryu Murakami"},
+                    new Author {Name = "Banana Yoshimoto"}
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
