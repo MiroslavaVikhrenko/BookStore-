2619306c522da7dc7d1df6b9acc646ecb08133ac
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-16 14:08:48 -0600

    Update set up to seed db correctly

--- BookStore/Data/DbInit.cs
+++ BookStore/Data/DbInit.cs
@@ -11,208 +11,125 @@ namespace BookStore.Data
     {
         public void Init(ApplicationContext context)
         {
+            if (!context.Categories.Any())
+            {
+                context.Categories.AddRange(new Category[]
+                {
+            new Category
+            {
+                Name = "Dreamlike & Surreal"
+            },
+            new Category
+            {
+                Name = "Love & Loneliness"
+            },
+            new Category
+            {
+                Name = "Psychological & Dark"
+            }
+                });
+                context.SaveChanges();
+            }
             if (!context.Authors.Any())
             {
                 context.Authors.AddRange(new Author[]
                 {
-                    new Author {Name = "Haruki Murakami" },
-                    new Author {Name = "Natsume Soseki"},
-                    new Author {Name = "Kawabata Yasunari"},
-                    new Author {Name = "Ryu Murakami"},
-                    new Author {Name = "Banana Yoshimoto"}
+            new Author { Name = "Jess Kidd"},
+            new Author { Name = "Martha McPhee"},
+            new Author { Name = "Megan Miranda"},
+            new Author { Name = "Helen Phillips"},
+            new Author { Name = "Karen Kingsbury"}
                 });
                 context.SaveChanges();
             }
-
             if (!context.Books.Any())
             {
+                Book theNightShip = new Book
+                {
+                    Title = "The Night Ship",
+                    Description = "Based on a real-life event, an epic historical novel from the award-winning author of Things in Jars.",
+                    Price = 70,
+                    Authors = new List<Author>()
+            {
+                context.Authors.FirstOrDefault(e => e.Name.Equals("Jess Kidd"))
+            },
+                    Category = context.Categories.FirstOrDefault(e => e.Name.Equals("Dreamlike & Surreal"))
+                };
+
+                Book theOnlySurvivors = new Book
+                {
+                    Title = "The Only Survivors",
+                    Description = "Thrilling mystery about a group of former classmates who reunite to mark the tenth anniversary of a tragic accident—only" +
+                    " to have one of the survivors disappear, casting fear and suspicion on the original tragedy.",
+                    Price = 59,
+                    Authors = new List<Author>()
+            {
+                context.Authors.FirstOrDefault(e=>e.Name.Equals("Megan Miranda")),
+                context.Authors.FirstOrDefault(e=>e.Name.Equals("Helen Phillips"))
+            },
+                    Category = context.Categories.FirstOrDefault(e => e.Name.Equals("Love & Loneliness"))
+                };
+
                 context.Books.AddRange(new Book[]
                 {
-                    new Book
-                    {
-                        Title = "Kafka on the Shore",
-                        Description = "A surreal and philosophical journey intertwining the lives of a runaway teen and an elderly man who talks to cats, blurring the lines between reality and dreams.",
-                        Price = 50.0m,
-                        Authors = new List<Author>()
-                        {
-                            context.Authors.FirstOrDefault(e => e.Name.Equals("Haruki Murakami"))
-                        }
-                    },
-                    new Book
-                    {
-                        Title = "Kitchen",
-                        Description = "A quietly powerful story about love, loss, and healing, centered around a young woman who finds comfort in cooking after the death of her grandmother.",
-                        Price = 40.0m,
-                        Authors = new List<Author>()
-                        {
-                            context.Authors.FirstOrDefault(e => e.Name.Equals("Banana Yoshimoto"))
-                        }
-                    },
-                    new Book
-                    {
-                        Title = "Kokoro",
-                        Description = "A poignant exploration of isolation, guilt, and the changing values of Meiji-era Japan, told through the complex relationship between a young man and a mysterious older mentor.",
-                        Price = 60.0m,
-                        Authors = new List<Author>()
-                        {
-                            context.Authors.FirstOrDefault(e => e.Name.Equals("Natsume Soseki"))
-                        }
-                    },
-                    new Book
-                    {
-                        Title = "Snow Country",
-                        Description = "A hauntingly lyrical tale of a doomed love affair between a Tokyo intellectual and a provincial geisha, set against the stark beauty of Japan’s snowy mountains.",
-                        Price = 70.0m,
-                        Authors = new List<Author>()
-                        {
-                            context.Authors.FirstOrDefault(e => e.Name.Equals("Kawabata Yasunari"))
-                        }
-                    },
-                    new Book
-                    {
-                        Title = "In the Miso Soup",
-                        Description = "A chilling psychological thriller following a Tokyo nightlife guide who suspects his American client may be a dangerous killer lurking in the city’s seedy underworld.",
-                        Price = 55.0m,
-                        Authors = new List<Author>()
-                        {
-                            context.Authors.FirstOrDefault(e => e.Name.Equals("Ryu Murakami"))
-                        }
-                    },
-                    new Book
-                    {
-                        Title = "Almost Transparent Blue",
-                        Description = "A raw, hallucinatory portrayal of disaffected youth in 1970s Japan, immersed in a world of sex, drugs, and existential aimlessness.",
-                        Price = 45.0m,
-                        Authors = new List<Author>()
-                        {
-                            context.Authors.FirstOrDefault(e => e.Name.Equals("Ryu Murakami"))
-                        }
-                    }
-
+           theNightShip,
+           theOnlySurvivors
                 });
+
                 context.SaveChanges();
             }
 
             if (!context.Reviews.Any())
             {
-                context.Reviews.AddRange(new Review[]
-                {
+                context.Reviews.AddRange
+                    (
                     new Review
                     {
-                        UserName = "Kenji",
-                        UserEmail = "kenji@gmail.com",
-                        Comment = "Haunting and beautifully written.",
-                        Stars = 4,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("Kafka on the Shore"))
-                    },
-                    new Review
-                    {
-                        UserName = "Yumi",
-                        UserEmail = "yui@gmail.com",
-                        Comment = "Couldn’t put it down.",
+                        UserName = "Alex",
+                        UserEmail = "alex@gmail.com",
+                        Comment = "Good book!",
                         Stars = 5,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("Kafka on the Shore"))
+                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("The Only Survivors"))
                     },
                     new Review
                     {
-                        UserName = "Naoko",
-                        UserEmail = "naoko@gmail.com",
-                        Comment = "Dark, raw, and unforgettable.",
+                        UserName = "Marry",
+                        UserEmail = "marry@gmail.com",
+                        Comment = "Nice to read.",
                         Stars = 4,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("Almost Transparent Blue"))
+                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("The Only Survivors"))
                     },
                     new Review
                     {
-                        UserName = "Makoto",
-                        UserEmail = "makoto@gmail.com",
-                        Comment = "Strange, but strangely moving.",
-                        Stars = 4,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("In the Miso Soup"))
-                    },
-                    new Review
-                    {
-                        UserName = "Taro",
-                        UserEmail = "taro@gmail.com",
-                        Comment = "Poetic and deeply human.",
+                        UserName = "John",
+                        UserEmail = "john@gmail.com",
+                        Comment = "Best thing I've ever read!",
                         Stars = 5,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("Kitchen"))
+                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("The Night Ship"))
                     }
-                });
+                    );
                 context.SaveChanges();
             }
-
             if (!context.Promotions.Any())
             {
-                context.Promotions.AddRange(new Promotion[]
-                {
+                context.Promotions.AddRange
+                    (
                     new Promotion
                     {
-                        Name = "Summer promotion",
-                        Percent = 10.0m,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("Kafka on the Shore"))
+                        Name = "Christmas Eve discount!",
+                        Percent = 15,
+                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("The Night Ship"))
                     },
-                    new Promotion
-                    {
-                        Name = "Golden week promotion",
-                        Amount = 5.0m,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("In the Miso Soup"))
-                    },
-                    new Promotion
-                    {
-                        Name = "Member promotion",
-                        Percent = 5.0m,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("Kitchen"))
-                    },
-                    new Promotion
-                    {
-                        Name = "First purchase promotion",
-                        Amount = 3.0m,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("Almost Transparent Blue"))
-                    },
-                    new Promotion
-                    {
-                        Name = "Happy customer promotion",
-                        Percent = 20.0m,
-                        Book = context.Books.FirstOrDefault(e => e.Title.Equals("Snow Country"))
-                    }
-                });
+                     new Promotion
+                     {
+                         Name = "Christmas Eve discount!",
+                         Amount = 25,
+                         Book = context.Books.FirstOrDefault(e => e.Title.Equals("The Only Survivors"))
+                     }
+                    );
                 context.SaveChanges();
             }
 
-            if (!context.Categories.Any())
-            {
-                context.Categories.AddRange(new Category[]
-                {
-                    new Category
-                    {
-                        Name = "Dreamlike & Surreal",
-                        Books = new List<Book>()
-                        {
-                            context.Books.FirstOrDefault(e => e.Title.Equals("Kafka on the Shore")),
-                            context.Books.FirstOrDefault(e => e.Title.Equals("Almost Transparent Blue"))
-                        }
-                    },
-                    new Category
-                    {
-                        Name = "Love & Loneliness",
-                        Books = new List<Book>()
-                        {
-                            context.Books.FirstOrDefault(e => e.Title.Equals("Kitchen")),
-                            context.Books.FirstOrDefault(e => e.Title.Equals("Snow Country"))
-                        }
-                    },
-                    new Category
-                    {
-                        Name = "Psychological & Dark",
-                        Books = new List<Book>()
-                        {
-                            context.Books.FirstOrDefault(e => e.Title.Equals("Kokoro")),
-                            context.Books.FirstOrDefault(e => e.Title.Equals("In the Miso Soup"))
-                        }
-                    },
-                });
-                context.SaveChanges();
-            }
         }
     }
 }
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,17 +1,45 @@
 ﻿using BookStore.Data;
+using BookStore.Interfaces;
+using BookStore.Models;
+using BookStore.Repositories;
 
 namespace BookStore
 {
-    internal class Program
-    {
-        public static ApplicationContext DbContext() => new ApplicationContextFactory().CreateDbContext(); 
-        static void Main(string[] args)
-        {
-            using (ApplicationContext db = DbContext())
-            {
-                new DbInit().Init(DbContext());
+    //internal class Program
+    //{
+    //    public static ApplicationContext DbContext() => new ApplicationContextFactory().CreateDbContext(); 
+    //    static void Main(string[] args)
+    //    {
+    //        //using (ApplicationContext db = DbContext())
+    //        //{
+    //        //    new DbInit().Init(DbContext());
 
+    //        //}
+
+    //    }
+    //}
+    public class Program
+    {
+        private static IBook _books;
+
+        public static ApplicationContext DbContext() => new ApplicationContextFactory().CreateDbContext();
+        static void Initialize()
+        {
+            new DbInit().Init(DbContext());
+            _books = new BookRepository();
+        }
+
+        static async Task Main()
+        {
+            Initialize();
+            var allBooks = await _books.GetAllBooksWithAuthorsAsync();
+            //Очищаем консоль от Sql команд
+            Console.Clear();
+            foreach (Book book in allBooks)
+            {
+                Console.WriteLine("{0}\nAuthors:\n{1}\n\n", book, String.Join("\n", book.Authors));
             }
         }
     }
+
 }
