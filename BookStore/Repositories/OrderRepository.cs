ab2a01ea3429c52bdfd2b5165d7755ffb145985c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-19 17:33:43 -0600

    Add empty OrderRepository that implements IOrder interface and OrderLineRepository that implements IOrderLine interface

---
+++ BookStore/Repositories/OrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repositories
+{
+    public class OrderRepository : IOrder
+    {
+    }
+}
