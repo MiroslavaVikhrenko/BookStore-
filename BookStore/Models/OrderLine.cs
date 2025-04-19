1c44757538a3f2d09e0a0ecbf64c1b0a50db2e3d
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-19 17:29:33 -0600

    Create OrderLine class

---
+++ BookStore/Models/OrderLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class OrderLine
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; } // FK
+        public int OrderId { get; set; } // FK
+        public int Quantity { get; set; }
+        public Book Book { get; set; } // nav prop
+        public Order Order { get; set; } // nav prop
+    }
+}
