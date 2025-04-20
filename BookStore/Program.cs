90fae53cc2243d0462c8ea4009761c7b1eb7c6b7
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-19 18:14:21 -0600

    Make Program class partial and initialize _orders and _authors

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -5,31 +5,35 @@ using BookStore.Repositories;
 
 namespace BookStore
 {
-    public class Program
+    partial class Program
     {
         enum ShopMenu
         {
             Books, Authors, Categories, Orders, SearchAuthor, SearchBooks, SearchCategories, SearchOrders, AddBook, AddAuthor, AddCategory, AddOrder, Exit
         }
         private static IBook _books;
+        private static IOrder _orders;
+        private static IAuthor _authors;
 
         public static ApplicationContext DbContext() => new ApplicationContextFactory().CreateDbContext();
         static void Initialize()
         {
             new DbInit().Init(DbContext());
             _books = new BookRepository();
+            _orders = new OrderRepository();
+            _authors = new AuthorRepository();
         }
 
         static async Task Main()
         {
             Initialize();
-            var allBooks = await _books.GetAllBooksWithAuthorsAsync();
-            //Очищаем консоль от Sql команд
-            Console.Clear();
-            foreach (Book book in allBooks)
-            {
-                Console.WriteLine("{0}\nAuthors:\n{1}\n\n", book, String.Join("\n", book.Authors));
-            }
+            //var allBooks = await _books.GetAllBooksWithAuthorsAsync();
+            ////Очищаем консоль от Sql команд
+            //Console.Clear();
+            //foreach (Book book in allBooks)
+            //{
+            //    Console.WriteLine("{0}\nAuthors:\n{1}\n\n", book, String.Join("\n", book.Authors));
+            //}
         }
     }
 
