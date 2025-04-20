59a6f56c074e9769730680a3707ca3e0369afc5c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-19 18:08:09 -0600

    Add IShow<T> interface

---
+++ BookStore/Interfaces/IShow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Interfaces
+{
+    public interface IShow<T> where T : INumber<T>
+    {
+        T Id { get; set; }
+        string value { get; set; }
+    }
+}
